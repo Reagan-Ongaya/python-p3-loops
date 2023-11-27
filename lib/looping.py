17e859d9679eed7dc886c0844cef45f4b9caaf4c
Reagan Ongaya <reagan.ongaya@student.moringaschool.com>
2023-11-28 01:17:31 +0300

    Asignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,38 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    
+    while countdown > 0:
+     print(countdown)
+     countdown -= 1
+    
+    print("Happy New Year!")
+
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
 
-def fizzbuzz():
-    # code goes here!
-    pass
+input_list = [1, 2, 3, 4, 5]
+result = square_integers(input_list)
+print(result)
+
+def fizzbuzz(num):
+    # code goes here 
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
+
+def fizzbuzz_printer():
+    for num in range(1, 101):
+        print(fizzbuzz(num))
+
+fizzbuzz_printer()
